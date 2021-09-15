c09b3acfb4d70243b7529c531133cbd10a8017f3
Dmitry Nezvinskyi <nezvinskyi@gmail.com>
2021-09-15 18:10:34 +0200

    adds gohome btn to about, contact, 404 pages

--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -1,3 +1,17 @@
-const Contact = () => <h1>This is Contact page</h1>;
+import { useHistory } from 'react-router-dom';
+import { Btn } from '../../components';
 
+const Contact = () => {
+	const history = useHistory();
+
+	const handleClickButton = () => {
+		history.push('/');
+	};
+	return (
+		<>
+			<h1>This is Contact page</h1>
+			<Btn title="Back To Home" onClick={handleClickButton} />
+		</>
+	);
+};
 export default Contact;
